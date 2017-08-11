ea0ded59801e9e69d880df2a8b97dd73e31ae8c1
rongxanh88 <rongxanh88@gmail.com>
2017-08-10 23:00:33 -0600

    removed semicolons from code

--- mergeSort.js
+++ mergeSort.js
@@ -1,34 +1,34 @@
 function mergeSort(numbers) {
   if (numbers.length === 1) {
-    return numbers;
+    return numbers
   }
-  let midpoint = Math.ceil(numbers.length/2);
-  let leftSide = numbers.slice(0, midpoint);
-  let rightSide = numbers.slice(midpoint, numbers.length);
-  let leftResult = mergeSort(leftSide);
-  let rightResult = mergeSort(rightSide);
-  return merge(leftResult, rightResult);
-};
+  let midpoint = Math.ceil(numbers.length/2)
+  let leftSide = numbers.slice(0, midpoint)
+  let rightSide = numbers.slice(midpoint, numbers.length)
+  let leftResult = mergeSort(leftSide)
+  let rightResult = mergeSort(rightSide)
+  return merge(leftResult, rightResult)
+}
 
 function merge(left, right) {
-  let mergedNumbers = [];
-  let index = 0;
+  let mergedNumbers = []
+  let index = 0
   while (left.length > 0 && right.length > 0) {
     if (left[index] < right[index]) {
-      mergedNumbers.push(left.shift());
+      mergedNumbers.push(left.shift())
     } else {
-      mergedNumbers.push(right.shift());
+      mergedNumbers.push(right.shift())
     }
   }
 
   while (left.length > 0) {
-    mergedNumbers.push(left.shift());
+    mergedNumbers.push(left.shift())
   }
 
   while (right.length > 0) {
-    mergedNumbers.push(right.shift());
+    mergedNumbers.push(right.shift())
   }
-  return mergedNumbers;
+  return mergedNumbers
 }
 
-module.exports = mergeSort;
+module.exports = mergeSort
