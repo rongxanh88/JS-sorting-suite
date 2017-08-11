00671c24d296bc01d2a6052f7e58255864916423
rongxanh88 <rongxanh88@gmail.com>
2017-08-10 22:55:48 -0600

    refactor tests to use arrow functions

--- test/bubbleSort-test.js
+++ test/bubbleSort-test.js
@@ -1,14 +1,14 @@
 const assert     = require('chai').assert
 const bubbleSort = require('../bubbleSort')
 
-describe('Bubble Sort', function() {
-  it('sorts three numbers', function() {
-    let numbers = [4,2,1];
-    assert.deepEqual(bubbleSort(numbers), [1,2,4]);
-  });
+describe('Bubble Sort', () => {
+  it('sorts three numbers', () => {
+    let numbers = [4,2,1]
+    assert.deepEqual(bubbleSort(numbers), [1,2,4])
+  })
 
-  it('sorts five numbers', function() {
+  it('sorts five numbers', () => {
     let numbers = [1,4,9,2,5];
     assert.deepEqual(bubbleSort(numbers), [1,2,4,5,9]);
   })
-});
+})
--- test/insertionSort-test.js
+++ test/insertionSort-test.js
@@ -1,14 +1,14 @@
 const assert        = require('chai').assert
 const insertionSort = require('../insertionSort')
 
-describe('Insertion Sort', function() {
-  it('sorts three numbers', function() {
-    let numbers = [4,2,1];
-    assert.deepEqual(insertionSort(numbers), [1,2,4]);
-  });
-
-  it('sorts five numbers', function() {
-    let numbers = [1,4,9,2,5];
-    assert.deepEqual(insertionSort(numbers), [1,2,4,5,9]);
+describe('Insertion Sort', () => {
+  it('sorts three numbers', () => {
+    let numbers = [4,2,1]
+    assert.deepEqual(insertionSort(numbers), [1,2,4])
   })
-});
+
+  it('sorts five numbers', () => {
+    let numbers = [1,4,9,2,5]
+    assert.deepEqual(insertionSort(numbers), [1,2,4,5,9])
+  })
+})
